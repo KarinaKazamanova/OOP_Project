10ab7ca33cdf6712b4187856d005c027126332ba
k-ter <k5524392@gmail.com>
2023-05-12 00:14:30 +0300

    OOP_Prodject

--- OOP_Prodject/Killer.java
+++ OOP_Prodject/Killer.java
@@ -3,7 +3,7 @@ package OOP_Prodject;
 public class Killer extends Profession {
 
     public Killer(String prof) {
-        super(prof, 10, 15, 10, 3, 2, "killerskill");
+        super(prof, 10, 15, 10, 3, 2);
         // TODO Auto-generated constructor stub
     }
 
